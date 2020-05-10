7323b15274b507b7b79541f02942113f8af469cb
jamesbrand <jamesbrand@gmail.com>
2020-05-10 10:09:09 +0800

    初始化文件

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![recursion_limit="2048"]
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+use yew::prelude::*;
+
+#[macro_use]
+mod common;
+mod components;
+mod layouts;
+mod router;
+
+use layouts::LayoutDefaults;
+
+/// 主函数
+fn main() {
+    //yew::initialize();
+    App::<LayoutDefaults>::new().mount_to_body();
+}
