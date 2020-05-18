63ffb702a846f37978c132209826d5c790edb475
jamesbrand <jamesbrand@gmail.com>
2020-05-18 09:14:53 +0800

    精简代码,优化代码

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,17 @@
 #![recursion_limit="2048"]
-//#[macro_use] extern crate log;
-//extern crate web_logger;
+#[macro_use] extern crate army_common;
 
 use wasm_bindgen::prelude::*;
+use yew::prelude::*;
 //#[global_allocator]
 //static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-use yew::prelude::*;
 
-#[macro_use]
-pub mod common;
 pub mod pages;
 pub mod layouts;
-//mod router;
 
-use layouts::LayoutDefaults;
 
 #[wasm_bindgen(start)]
 pub async fn run_app() {
     // web_logger::init();
-    App::<LayoutDefaults>::new().mount_to_body();
+    App::<layouts::LayoutDefaults>::new().mount_to_body();
 }
