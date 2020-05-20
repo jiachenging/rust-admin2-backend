00f02962331174bff755d8a6f6840d2d02b35c04
jamesbrand <jamesbrand@gmail.com>
2020-05-20 21:23:20 +0800

    增加用户处理相关

--- src/pages/users/users.rs
+++ src/pages/users/users.rs
@@ -1,17 +1,67 @@
 use yew::prelude::*;
+use yew::services::fetch::{FetchTask};
+use lucky::{
+    json::JsonPager,
+    crypt,
+    models::{UserRow},
+    types::SwapData,
+    web::{FetchMsg, Console, Api},
+};
 
-pub struct Users { }
+pub struct Users {
+    fetching: bool,
+    #[allow(dead_code)]
+    link: ComponentLink<Self>,
+    #[allow(dead_code)]
+    task: Option<FetchTask>,
+}
+
+impl Users {
+    pub fn test() {
+        Console::log("测试 test!");
+    }
+}
 
 impl Component for Users {
 
-    type Message = ();
+    type Message = FetchMsg<SwapData>;
     type Properties = ();
 
-    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self { }
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self::test();
+        Self {
+            task: Some(Api::send("/users", &link)),
+            fetching: true,
+            link,
+        }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            FetchMsg::FetchData => {
+                if self.fetching { //如果正在获取数据
+                    return false;
+                }
+            },
+            FetchMsg::FetchReady(response) => {
+                if let Some(r) = response.ok() {
+                    let result = if let Ok(v) = crypt::decrypt::<JsonPager<UserRow>>(&r) { v } else {
+                        Console::error("对于返回信息进行解密失败");
+                        return false;
+                    };
+                    let message = format!("pager: {:?}", result);
+                    Console::log(&message);
+                    Console::log_object(&result);
+                    if let Some(rows) = result.rows {
+                        bind_data!(&rows, [id, name, login_count, last_login, last_ip, status, created, updated]);
+                    }
+                    self.fetching = false;
+                }
+            },
+            _ => {
+                return false;
+            }
+        }
         true
     }
 
