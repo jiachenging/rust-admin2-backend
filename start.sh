30e694260016ea7790fe6334f560733f211f1308
jamesbrand <jamesbrand@gmail.com>
2020-05-21 11:22:06 +0800

    为提高编译速度，暂时使用dev模式进行开发

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wasm-pack build --target web --out-name wasm --out-dir ./pkg
+#wasm-pack build --target web --out-name wasm --out-dir ./pkg
+wasm-pack build --target web --out-name wasm --out-dir ./pkg --dev
