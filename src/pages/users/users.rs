806d95a589b95157fdd2e7815c7d3ac0a61ad70e
jamesbrand <jamesbrand@gmail.com>
2020-05-21 11:20:29 +0800

    修正代码, 将视力渲染部分写到 view 函数里面

--- src/pages/users/users.rs
+++ src/pages/users/users.rs
@@ -1,12 +1,14 @@
 use yew::prelude::*;
 use yew::services::fetch::{FetchTask};
+use yew::virtual_dom::{VNode, VList};
 use lucky::{
     json::JsonPager,
     crypt,
     models::{UserRow},
     types::SwapData,
-    web::{FetchMsg, Console, Api},
+    web::{FetchMsg, Console, Api, LocalStorage},
 };
+use js_sys::Date;
 
 pub struct Users {
     fetching: bool,
@@ -14,12 +16,7 @@ pub struct Users {
     link: ComponentLink<Self>,
     #[allow(dead_code)]
     task: Option<FetchTask>,
-}
-
-impl Users {
-    pub fn test() {
-        Console::log("测试 test!");
-    }
+    pager: Option<JsonPager<UserRow>>,
 }
 
 impl Component for Users {
@@ -28,11 +25,16 @@ impl Component for Users {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self::test();
+        let pager = if let Some(v) = LocalStorage::get::<JsonPager<UserRow>>("users") {
+            if Date::now() / 1000.0 - v.time < 30.0 { //如果缓存在30秒之内
+                Some(v)
+            } else { None }
+        } else { None };
         Self {
-            task: Some(Api::send("/users", &link)),
-            fetching: true,
+            task: if pager.is_none() { Some(Api::send("/users", &link)) } else { None },
+            fetching: pager.is_none(), //pager.is_none(), //true,
             link,
+            pager,
         }
     }
 
@@ -44,19 +46,22 @@ impl Component for Users {
                 }
             },
             FetchMsg::FetchReady(response) => {
-                if let Some(r) = response.ok() {
-                    let result = if let Ok(v) = crypt::decrypt::<JsonPager<UserRow>>(&r) { v } else {
-                        Console::error("对于返回信息进行解密失败");
-                        return false;
-                    };
-                    let message = format!("pager: {:?}", result);
-                    Console::log(&message);
-                    Console::log_object(&result);
-                    if let Some(rows) = result.rows {
-                        bind_data!(&rows, [id, name, login_count, last_login, last_ip, status, created, updated]);
-                    }
+                let data = if let Ok(v) = response { v } else {
+                    Console::error("获取服务端返回的数据时出错");
                     self.fetching = false;
-                }
+                    return false;
+                };
+                let pager = if let Ok(mut v) = crypt::decrypt::<JsonPager<UserRow>>(&data) {
+                    v.time = Date::now() / 1000.0;
+                    LocalStorage::set::<JsonPager<UserRow>>("users", &v);
+                    Some(v)
+                } else {
+                    Console::error("对于返回信息进行解密失败");
+                    self.fetching = false;
+                    return false;
+                };
+                self.pager = pager;
+                self.fetching = false;
             },
             _ => {
                 return false;
@@ -69,7 +74,28 @@ impl Component for Users {
         false
     }
 
-    fn view(&self) -> Html {
+    fn view(&self) -> VNode {
+        let mut rows = VList::new();
+        if let Some(p) = &self.pager {
+            if let Some(rs) = &p.rows {
+                for r in rs {
+                    rows.add_child(html!{
+                        <tr>
+                            <td>{r.id}</td>
+                            <td>{&r.name}</td>
+                            <td>{&r.login_count}</td>
+                            <td>{&r.last_login}</td>
+                            <td>{&r.last_ip}</td>
+                            <td>{if r.status == 1 { "正常"} else { "停用"}}</td>
+                            <td>{&r.created}</td>
+                            <td>{&r.updated}</td>
+                            <td>{"修改 | 删除"}</td>
+                        </tr>
+                    });
+                }
+            }
+        }
+
         render_view!("/users/users.html")
     }
 }
