a192d9d923f5735b14d0045a9b812356a66245d3
flip <jamesbrand@gmail.com>
2020-05-22 18:51:02 +0800

    修复翁化

--- src/pages/systems/provinces.rs
+++ src/pages/systems/provinces.rs
@@ -1,17 +1,60 @@
+
 use yew::prelude::*;
+use yew::services::fetch::{FetchTask};
+use yew::virtual_dom::{VNode};
+use lucky::{
+    json::JsonPager,
+    models::{ProvinceRow as ModelRow},
+    types::SwapData,
+    web::{FetchMsg, Api},
+    html::DataTable,
+};
 
-pub struct Provinces { }
+const API_LIST: &'static str = "/provinces";
+const API_CACHE_KEY: &'static str = "provinces";
 
-impl Component for Provinces{
+pub struct Provinces {
+    fetching: bool,
+    #[allow(dead_code)]
+    link: ComponentLink<Self>,
+    #[allow(dead_code)]
+    task: Option<FetchTask>,
+    pager: Option<JsonPager<ModelRow>>,
+}
 
-    type Message = ();
+impl DataTable for Provinces {}
+
+impl Component for Provinces {
+
+    type Message = FetchMsg<SwapData>;
     type Properties = ();
 
-    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self { }
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let pager = Self::get_pager::<ModelRow>(API_CACHE_KEY);
+        Self {
+            task: if pager.is_none() { Some(Api::send(API_LIST, &link)) } else { None },
+            fetching: pager.is_none(), //pager.is_none(), //true,
+            link,
+            pager,
+        }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            FetchMsg::FetchData => {
+                if self.fetching { //如果正在获取数据
+                    return false;
+                }
+            },
+            FetchMsg::FetchReady(response) => {
+                let pager = Self::fetch_pager::<ModelRow>(response, API_CACHE_KEY);
+                self.pager = pager;
+                self.fetching = false;
+            },
+            _ => {
+                return false;
+            }
+        }
         true
     }
 
@@ -19,7 +62,18 @@ impl Component for Provinces{
         false
     }
 
-    fn view(&self) -> Html {
+    fn view(&self) -> VNode {
+        let rows = Self::init_rows(&self.pager, |r: &ModelRow| {
+            return html!{
+                <tr>
+                    <td>{&r.id}</td>
+                    <td>{&r.name}</td>
+                    <td>{&r.code}</td>
+                    <td>{&r.remark}</td>
+                    <td>{&r.sort}</td>
+                </tr>
+            };
+        });
         render_view!("/systems/provinces.html")
     }
 }
