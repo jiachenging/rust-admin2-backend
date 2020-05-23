54a40fd4962bb4a2beb9328d7ec4bbb2f5ed502a
flip <jamesbrand@gmail.com>
2020-05-23 21:17:37 +0800

    修改调整文件: src/pages/finances/account_changes.rs

--- src/pages/finances/account_changes.rs
+++ src/pages/finances/account_changes.rs
@@ -1,17 +1,59 @@
 use yew::prelude::*;
+use yew::services::fetch::{FetchTask};
+use yew::virtual_dom::{VNode};
+use lucky::{
+    json::JsonPager,
+    models::{AccountChangeRow as ModelRow},
+    types::SwapData,
+    web::{FetchMsg, Api},
+    html::DataTable,
+};
 
-pub struct AccountChanges { }
+const API_LIST: &'static str = "/account_changes";
+const API_CACHE_KEY: &'static str = "account_changes";
+
+pub struct AccountChanges {
+    fetching: bool,
+    #[allow(dead_code)]
+    link: ComponentLink<Self>,
+    #[allow(dead_code)]
+    task: Option<FetchTask>,
+    pager: Option<JsonPager<ModelRow>>,
+}
+
+impl DataTable for AccountChanges {}
 
 impl Component for AccountChanges {
 
-    type Message = ();
+    type Message = FetchMsg<SwapData>;
     type Properties = ();
 
-    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self { }
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let pager = Self::get_pager::<ModelRow>(API_CACHE_KEY);
+        Self {
+            task: if pager.is_none() { Some(Api::send(API_LIST, &link)) } else { None },
+            fetching: pager.is_none(), //pager.is_none(), //true,
+            link,
+            pager,
+        }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            FetchMsg::FetchData => {
+                if self.fetching { //如果正在获取数据
+                    return false;
+                }
+            },
+            FetchMsg::FetchReady(response) => {
+                let pager = Self::fetch_pager::<ModelRow>(response, API_CACHE_KEY);
+                self.pager = pager;
+                self.fetching = false;
+            },
+            _ => {
+                return false;
+            }
+        }
         true
     }
 
@@ -19,7 +61,23 @@ impl Component for AccountChanges {
         false
     }
 
-    fn view(&self) -> Html {
+    fn view(&self) -> VNode {
+        let rows = Self::init_rows(&self.pager, |r: &ModelRow| {
+            return html!{
+                <tr>
+                    <td>{&r.id}</td>
+                    <td>{&r.bill_no}</td>
+                    <td>{&r.user_id}</td>
+                    <td>{&r.user_name}</td>
+                    <td>{&r.in_out}</td>
+                    <td>{&r.amount}</td>
+                    <td>{&r.before_total}</td>
+                    <td>{&r.after_total}</td>
+                    <td>{&r.created}</td>
+                    <td>{&r.remark}</td>
+                </tr>
+            };
+        });
         render_view!("/finances/account_changes.html")
     }
 }
